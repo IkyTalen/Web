f4a5ee2212aba694730cdc072d396a4ccc2477f1
IkyTalen <masiky621@gmail.com>
2026-04-24 11:07:11 +0700

    Update data.js

--- data.js
+++ data.js
@@ -17,6 +17,15 @@ const databaseProyek = {
         pelajaran: "seluk-beluk hardware (perangkat keras) dan software (perangkat lunak) komputer, serta perancangan dan administrasi jaringan komputer",
         link: "https://kelastkj.smkn1telagasari.sch.id/"
     },
+     "jurusan_dkv": {
+        judul: "Jurusan Desain Komunikasi Visual",
+        keahlian: ["Desain Grafis", "Ilustrasi", "Fotografi"],
+        gambar: "https://sevima.com/wp-content/uploads/2021/10/jurusan-dkv.jpg",
+        latar: " berakar pada kebutuhan untuk menyampaikan pesan, ide, atau informasi kompleks secara efektif, menarik, dan mudah dipahami melalui elemen visual ",
+        tantangan: "keharusan beradaptasi dengan teknologi AI yang cepat, memahami selera klien yang dinamis, serta mengatasi misinterpretasi pesan",
+        pelajaran: "studi tentang penyampaian pesan, ide, atau informasi secara efektif, kreatif, dan informatif melalui media visual seperti grafis, ilustrasi, fotografi, videografi, dan animasi ",
+        link: "https://campus.quipper.com/majors/id-desain-komunikasi-visual"
+    },
      "jurusan_tp": {
         judul: "Jurusan Teknik Pemesinan",
         keahlian: ["Pemesinan Konvensional", "Pemesinan CNC", "Pemeliharaan Mesin"],
@@ -25,6 +34,16 @@ const databaseProyek = {
         tantangan: "berfokus pada adaptasi terhadap Industri 4.0, meliputi otomatisasi, kebutuhan presisi tinggi, dan efisiensi biaya",
         pelajaran: "mempelajari cara memproduksi komponen mesin dengan presisi menggunakan alat konvensional (bubut, frais, gerinda) dan CNC ",
         link: "https://books.google.co.id/books/about/Pekerjaan_Dasar_Teknik_Mesin_SMK_MAK_Kel.html?id=8BEpEAAAQBAJ&redir_esc=y"
+    },
+      
+      "jurusan_tkr": {
+        judul: "Jurusan Teknik Kendaraan Ringan",
+        keahlian: ["Pemeliharaan Mesin Kendaraan Ringan", "Pemeliharaan Kelistrikan Kendaraan Ringan", "Dasar-Dasar Otomotif"],
+        gambar: "https://smkmbandongan.sch.id/media_library/posts/post-image-1572923213673.png",
+        latar: " bidang keahlian di SMK yang mempelajari perawatan, perbaikan, dan diagnosis kendaraan bermotor roda empat ",
+        tantangan: "berfokus pada adaptasi teknologi tinggi, meliputi elektrifikasi (kendaraan listrik), sistem kelistrikan kompleks, standar emisi global (Euro 5/6), serta kebutuhan diagnostik berbasis komputer",
+        pelajaran: "fokus utama pembelajaran adalah pada perawatan, perbaikan, dan diagnosa kendaraan roda empat (mobil, van, pick-up) serta terkadang roda dua ",
+        link: "https://www.quipper.com/id/blog/quipper-campus/n-jurusan-teknik-kendaraan-ringan-tkr/"
     },
     
 };
