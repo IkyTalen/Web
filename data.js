e75b37e93e9fd4937589308cef0a2cd4a2e61408
IkyTalen <masiky621@gmail.com>
2026-04-24 09:22:33 +0700

    upload pertama ku ya kan belajar awak
    ajarin dong puh sepuh,masik pemula ni

---
+++ data.js
@@ -0,0 +1,30 @@
+const databaseProyek = {
+    "jurusan_rpl": {
+        judul: "Jurusan Rekayasa Perangkat Lunak",
+        keahlian: ["Pemrograman", "Pengembangan Web", "Manajemen Basis Data", "UI/UX Design"],
+        gambar: "https://s3.ap-southeast-1.amazonaws.com/maukuliah/major/1702537940-yTDL9TpQ6r.jpg",
+        latar: "kebutuhan untuk memecahkan masalah kompleks melalui teknologi secara efisien, andal, dan terstruktur",
+        tantangan: "perubahan kebutuhan klien yang cepat, tenggat waktu ketat, keamanan data, dan kolaborasi tim",
+        pelajaran: "mempelajari prinsip dan teknik pengembangan software, mulai dari desain, coding, pengujian, hingga pemeliharaan.",
+        link: "https://csirt.teknokrat.ac.id/step-by-step-belajar-rekayasa-perangkat-lunak-dari-nol/"
+    },
+    "jurusan_tkj": {
+        judul: "Jurusan Teknik Komputer dan Jaringan",
+        keahlian: ["Perangkat Keras Komputer", "Sistem Operasi", "Pengelolaan Jaringan"],
+        gambar: "https://www.uib.ac.id/wp-content/uploads/2025/04/pengertian-jaringan-komputer-jenis-cara-kerja-d.png",
+        latar: "kebutuhan mendesak untuk meningkatkan efisiensi operasional, berbagi data secara real-time, dan mempermudah komunikasi antar-pengguna",
+        tantangan: "Tantangan utama TKJ = keamanan + kompleksitas + perubahan cepat.Solusi utama = skill + tools + sistem kerja yang rapi",
+        pelajaran: "seluk-beluk hardware (perangkat keras) dan software (perangkat lunak) komputer, serta perancangan dan administrasi jaringan komputer",
+        link: "https://kelastkj.smkn1telagasari.sch.id/"
+    },
+     "jurusan_tp": {
+        judul: "Jurusan Teknik Pemesinan",
+        keahlian: ["Pemesinan Konvensional", "Pemesinan CNC", "Pemeliharaan Mesin"],
+        gambar: "https://www.smkn1jenpo.sch.id/wp-content/uploads/2025/04/IMG_5391_11zon-scaled.jpg",
+        latar: "berfokus pada pentingnya proses manufaktur presisi (bubut, frais, CNC) dalam menghasilkan komponen berkualitas tinggi",
+        tantangan: "berfokus pada adaptasi terhadap Industri 4.0, meliputi otomatisasi, kebutuhan presisi tinggi, dan efisiensi biaya",
+        pelajaran: "mempelajari cara memproduksi komponen mesin dengan presisi menggunakan alat konvensional (bubut, frais, gerinda) dan CNC ",
+        link: "https://books.google.co.id/books/about/Pekerjaan_Dasar_Teknik_Mesin_SMK_MAK_Kel.html?id=8BEpEAAAQBAJ&redir_esc=y"
+    },
+    
+};
